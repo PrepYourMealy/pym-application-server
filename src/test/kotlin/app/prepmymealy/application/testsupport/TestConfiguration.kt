e1b95a4314dbed8efa833525533f0867487017af
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-12 14:59:55 +0200

    #2 add github action

--- src/test/kotlin/app/prepmymealy/application/testsupport/TestConfiguration.kt
+++ src/test/kotlin/app/prepmymealy/application/testsupport/TestConfiguration.kt
@@ -5,9 +5,12 @@ import com.mongodb.client.MongoClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
+import org.springframework.ai.openai.OpenAiChatModel
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @Configuration
 class TestConfiguration {
+
     @Bean(name = ["defaultClock"])
     fun defaultClock(): Clock {
         return Clock.systemDefaultZone()
@@ -17,4 +20,7 @@ class TestConfiguration {
     fun mongoClient(container: MongoDbTestContainer): MongoClient {
         return MongoClients.create(container.getConnectionString())
     }
+
+    @MockBean(name = "openAiChatModel")
+    private lateinit var openAiChatModel: OpenAiChatModel
 }
