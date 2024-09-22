e8f56ca1f75a042beb5a687dd16e7d7307fa0e21
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-22 19:45:42 +0200

    add bought item to shoppinglist

--- src/main/kotlin/app/prepmymealy/application/representation/ShoppingListItemRepresentation.kt
+++ src/main/kotlin/app/prepmymealy/application/representation/ShoppingListItemRepresentation.kt
@@ -1,6 +1,8 @@
 package app.prepmymealy.application.representation
 
-data class ShoppingListItemRepresentation(
+import com.fasterxml.jackson.annotation.JsonCreator
+
+data class ShoppingListItemRepresentation @JsonCreator constructor(
     val name: String,
     val price: Double,
     val quantity: Int,
--- src/main/kotlin/app/prepmymealy/application/representation/ShoppingListRepresentation.kt
+++ src/main/kotlin/app/prepmymealy/application/representation/ShoppingListRepresentation.kt
@@ -1,6 +1,8 @@
 package app.prepmymealy.application.representation
 
-data class ShoppingListRepresentation(
+import com.fasterxml.jackson.annotation.JsonCreator
+
+data class ShoppingListRepresentation @JsonCreator constructor(
     val total: Double,
     val items: List<ShoppingListItemRepresentation>,
 )
